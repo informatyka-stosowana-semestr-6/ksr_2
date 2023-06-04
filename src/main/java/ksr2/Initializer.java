7d45847fa9c36917c792ec2d2d2f3567ecdd9029
balnabu <przemek_lis@onet.eu>
2023-06-04 21:33:45 +0200

    add factory

--- src/main/java/ksr2/Initializer.java
+++ src/main/java/ksr2/Initializer.java
@@ -1,25 +1,62 @@
 package ksr2;
 
+import ksr2.logic.FuzzySet;
+import ksr2.logic.Label;
 import ksr2.logic.LinguisticVariable;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Iterator;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+
+import ksr2.logic.membershipFunction.MembershipFunction;
+import ksr2.logic.membershipFunction.MembershipFunctionFactory;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 
 public class Initializer {
-    public List<LinguisticVariable> createLinguisticVariable() throws IOException, ParseException {
+    public List<LinguisticVariable> createLinguisticVariable() {
         List<LinguisticVariable> linguisticVariables = new ArrayList<>();
-        JSONParser parser = new JSONParser();
-        Object obj = parser.parse(new FileReader("D:\\studia\\semestr_8\\KSR\\ksr_2\\src\\main\\resources\\linguisticsVariables.json"));
-        JSONObject jsonObject =  (JSONObject) obj;
-//        TODO
+
+        try {
+//            TODO
+//            String filePath = Paths.get(System.getProperty("user.dir"), "resources/linguisticsVariables.json").toString();
+            String filePath = "D:\\studia\\semestr_8\\KSR\\ksr_2\\src\\main\\resources\\linguisticsVariables.json";
+            String jsonContent = new String(Files.readAllBytes(Paths.get(filePath)));
+            JSONObject data = new JSONObject(jsonContent);
+
+            for (String key : data.keySet()) {
+                JSONObject value = data.getJSONObject(key);
+                String function = value.getString("function");
+
+                JSONArray variables = value.getJSONArray("variables");
+                List<Label> labelList = new ArrayList<>();
+
+                for (int i = 0; i < variables.length(); i++) {
+                    JSONObject variableValue = variables.getJSONObject(i);
+                    String variableName = variableValue.keys().next();
+                    JSONArray variableArray = variableValue.getJSONArray(variableName);
+
+                    List<Double> variableValues = new ArrayList<>();
+                    for (int j = 0; j < variableArray.length(); j++) {
+                        variableValues.add(variableArray.getDouble(j));
+                    }
+
+                    MembershipFunction membershipFunction = MembershipFunctionFactory.createObject(function, variableValues);
+                    FuzzySet fuzzySet = new FuzzySet(membershipFunction);
+                    Label label = new Label(variableName, fuzzySet);
+                    labelList.add(label);
+                }
+
+                LinguisticVariable linguisticVariable = new LinguisticVariable(key, labelList);
+                linguisticVariables.add(linguisticVariable);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return linguisticVariables;
     }
 }
+
--- src/main/java/ksr2/Main.java
+++ src/main/java/ksr2/Main.java
@@ -1,4 +1,13 @@
 package ksr2;
 
+import ksr2.logic.LinguisticVariable;
+
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        Initializer initializer = new Initializer();
+        List<LinguisticVariable> linguisticVariables = initializer.createLinguisticVariable();
+
+    }
 }
